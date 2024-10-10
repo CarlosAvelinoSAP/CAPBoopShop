3791a7c67acc757b56398a22eff9527a3e049257
CARLOS Jose Avelino <carlos.avelino.prestserv@petrobras.com.br>
2024-10-10 18:51:29 +0000

    Fixed action event in cat.js

--- srv/cat.js
+++ srv/cat.js
@@ -6,11 +6,27 @@ class CatalogService extends cds.ApplicationService {
         const { Books } = this.entities;
         this.after('READ', CatalogService.Authors, results => {
             console.log(`****/////******`);
-            this.grantDiscount(results);
+            this.grantDiscount(results);       
         })
+
+        this.on('submitOrder', this.reduceStock);
 
     return super.init();
     };
+
+//As an alternative to registering an on handler, actions and functions can also be implemented 
+//as conventional JavaScript methods on a service implementation class
+/*     submitOrder(book, quantity) {
+        const { Books } = this.entities;
+    
+        if (quantity < 1) {
+          return req.error('The quantity must be at least 1.');
+        }
+    
+        let stock = 10;
+        return { stock };
+      } */
+    
     grantDiscount(results){
         for(let b of results){
             if (b.stock > 20) {
@@ -19,7 +35,16 @@ class CatalogService extends cds.ApplicationService {
         }
     }
 
-
+    reduceStock(req){
+        const { Books } = this.entities;
+        const { book, quantity } = req.data;
+        console.log(`reduceStock!!`)
+        if ( quantity < 1 ) {
+            return req.reject(410, `The quantity must be at least 1.`);
+        }
+        let stock = 10;
+        return {stock};
+    }
 }
 
 module.exports = CatalogService;
