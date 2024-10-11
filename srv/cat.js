3d7a7bdf57fdb79a5e413f54309558d6d87673db
CARLOS Jose Avelino <carlos.avelino.prestserv@petrobras.com.br>
2024-10-11 00:07:16 +0000

    Fixed reduceStock action function

--- srv/cat.js
+++ srv/cat.js
@@ -6,44 +6,81 @@ class CatalogService extends cds.ApplicationService {
         const { Books } = this.entities;
         this.after('READ', CatalogService.Authors, results => {
             console.log(`****/////******`);
-            this.grantDiscount(results);       
+            this.grantDiscount(results);
         })
 
         this.on('submitOrder', this.reduceStock);
 
-    return super.init();
+        return super.init();
     };
 
-//As an alternative to registering an on handler, actions and functions can also be implemented 
-//as conventional JavaScript methods on a service implementation class
-/*     submitOrder(book, quantity) {
-        const { Books } = this.entities;
-    
-        if (quantity < 1) {
-          return req.error('The quantity must be at least 1.');
-        }
-    
-        let stock = 10;
-        return { stock };
-      } */
-    
-    grantDiscount(results){
-        for(let b of results){
+    //As an alternative to registering an on handler, actions and functions can also be implemented 
+    //as conventional JavaScript methods on a service implementation class
+    /*     submitOrder(book, quantity) {
+            const { Books } = this.entities;
+        
+            if (quantity < 1) {
+              return req.error('The quantity must be at least 1.');
+            }
+        
+            let stock = 10;
+            return { stock };
+          } */
+
+    grantDiscount(results) {
+        for (let b of results) {
             if (b.stock > 20) {
-                b.title  += ' -- 11% Discount!';
+                b.title += ' -- 11% Discount!';
             }
         }
     }
 
-    reduceStock(req){
+    /*async reduceStock(req){
         const { Books } = this.entities;
         const { book, quantity } = req.data;
-        console.log(`reduceStock!!`)
+
+        //  let query1 = SELECT.one.from(Books).where({ ID: book }).columns( b=> { b.stock } );
+        //let query1 = SELECT.one.from(Books).where`ID=${book}`.columns`stock`;
+        //await SELECT.one.from(Books).where`ID=${book}`.columns`stock`;
+        //let query2 = UPDATE(Books).where({ID:book}).with({stock: { '-=' : quantity }});
+        //let query2 = UPDATE(Books).where`ID=${book}`.with`stock = stock - ${quantity}` ;
+        //await UPDATE(Books).where`ID=${book}`.with`stock = stock - ${quantity}` ;
+        const b = await SELECT.one.from(Books).where({ ID: book }).columns( b=> { b.stock } );
+        let { stock } = b;
+        await UPDATE(Books).where({ID:book}).with({stock: { '-=' : quantity }});
+        return { stock: stock - quantity };
+    }*/
+
+    /*   * console.log(`reduceStock!!`)
         if ( quantity < 1 ) {
             return req.reject(410, `The quantity must be at least 1.`);
         }
-        let stock = 10;
-        return {stock};
+        console.log(`TTTTTTTTTTTAAAAAAAAAAAA!!!!!`)
+        //return { stock };
+        return { stock: stock - quantity };
+    }*/
+    async reduceStock(req) {
+        const { Books } = this.entities;
+        
+        const { book, quantity } = req.data;
+
+        if (quantity < 1) {
+            return req.error('The quantity must be at least 1.');
+        }
+
+        const b = await SELECT.one.from(Books).where({ ID: book }).columns(b => { b.stock });
+
+        if (!b) {
+            return req.error(`Book with ID ${book} does not exist.`);
+        }
+
+        let { stock } = b;
+        if (quantity > stock) {
+            return req.error(`${quantity} exceeds stock ${stock} for book with ID ${book}.`);
+        }
+
+        await UPDATE(Books).where({ ID: book }).with({ stock: { '-=': quantity } });
+        return { stock: stock - quantity };
     }
 }
 
